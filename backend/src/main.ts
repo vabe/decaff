66f2449f9ddc894c8ef9135c5f6a2193a01afd62
László Sipula <sipulalaci@gmail.com>
2022-10-23 13:35:53 +0200

    feat: add startup logs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('deCaff')
+    .setDescription('The secure special image marketplace')
+    .setVersion('1.0')
+    .addTag('caff')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+
+  const port = 3000;
+
+  await app.listen(port);
+
+  Logger.log(`8====================================D`);
+  Logger.log(`Application is running on: http://localhost:${port}`);
+  Logger.log(`Swagger is available on: http://localhost:${port}/api/swagger`);
 }
+
 bootstrap();

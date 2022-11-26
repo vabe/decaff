9b288a1ff6041533914b7a55bccd8112f159f518
Bence Valent <vabe97@gmail.com>
2022-11-26 14:08:41 +0100

    feat(backend): add firebase basic include

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
+import { AuthMiddleware } from "src/auth/auth.middleware";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes({
+      path: "*",
+      method: RequestMethod.ALL,
+    });
+  }
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,26 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app/app.module';
-import { Logger } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app/app.module";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('deCaff')
-    .setDescription('The secure special image marketplace')
-    .setVersion('1.0')
-    .addTag('caff')
+    .setTitle("deCAFF")
+    .setDescription("The secure special image marketplace")
+    .setVersion("1.0")
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/swagger', app, document);
+  SwaggerModule.setup("api/swagger", app, document);
 
   const port = 3000;
 
   await app.listen(port);
 
-  Logger.log(`8====================================D`);
+  Logger.log("8====================================D");
   Logger.log(`Application is running on: http://localhost:${port}`);
   Logger.log(`Swagger is available on: http://localhost:${port}/api/swagger`);
 }

839dbb66f0fe52cce8df01d529efc5baf7777e23
Bence Valent <vabe97@gmail.com>
2022-11-26 14:08:41 +0100

    feat(backend): add firebase basic include

--- backend/src/app/app.controller.ts
+++ backend/src/app/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Req } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/hi")
+  getHi(@Req() request: any): string {
+    return `Hi ${request["user"]?.email}`;
   }
 }

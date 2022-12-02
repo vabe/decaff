ffd3b00cf7bc9738bfcdb128827b4ba87fc17446
Bence Valent <vabe97@gmail.com>
2022-12-02 18:37:19 +0100

    fix(backend): update cors and prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from "./app/app.module";
 import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.setGlobalPrefix("api");
 
   const config = new DocumentBuilder()
     .addBearerAuth()

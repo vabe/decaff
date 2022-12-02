f2e75028f5fddac27160978894e6db58f39cf38d
Bence Valent <vabe97@gmail.com>
2022-12-02 02:12:24 +0100

    feat(frontend): update integration from backend

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,36 @@
-import { FirestoreAdapter } from "@next-auth/firebase-adapter";
+import axios from "axios";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { firebaseConfig } from "@/utils/firebase-config";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    CredentialsProvider({
+      name: "deCAuFF",
+      credentials: {
+        email: {
+          label: "Email address",
+          type: "email",
+          placeholder: "claire@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const res = await axios.post(
+          "http://localhost:3010/auth/login",
+          JSON.stringify(credentials),
+          { headers: { "Content-Type": "application/json" } }
+        );
+
+        const user = res.data;
+
+        if (res.status < 300 && user) {
+          return user;
+        }
+
+        return null;
+      },
     }),
   ],
-  adapter: FirestoreAdapter(firebaseConfig),
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/auth/sign-in",

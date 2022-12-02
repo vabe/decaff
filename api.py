ba719b59bc2f7073ab06ac9d39f221b9a91ba1f0
Zsomborv <zsombor.varga4@gmail.com>
2022-12-02 04:52:02 +0100

    feat: api

--- api.py
+++ api.py
@@ -15,9 +15,7 @@ def upload_file():
     if request.method == 'POST':
         data = request.get_json()
         f = wrapper.parse(data['upload_path'], data['json_name'])
-        print("itt?1")
-        file_path = f"cpp_parser/json/{data['json_name']}.json"
-        return_json = None
+        file_path = f"{data['upload_path']}/{data['json_name']}.json"
         f2 = open(file_path)
         return_json = json.load(f2)
         return return_json

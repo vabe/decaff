53dace7f65dbdd39be913b7fedc035883b53ba45
Bence Valent <vabe97@gmail.com>
2022-12-03 22:26:09 +0100

    feat(frontend): include security headers

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,27 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=1800; includeSubDomains; preload",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
